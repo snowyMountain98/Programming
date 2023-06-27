9791f2dd37b37c1e8cbb4784cd1f2bd7273befd5
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-06-27 17:59:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,12 +1,11 @@
 class Solution {
 	public int solution(int price) {
-		if (price >= 500000) {
+		if (price >= 500000)
 			price *= 0.8;
-		} else if (price >= 300000) {
+		else if (price >= 300000)
 			price *= 0.9;
-		} else if (price >= 100000) {
+		else if (price >= 100000)
 			price *= 0.95;
-		}
 
 		return price;
 	}
