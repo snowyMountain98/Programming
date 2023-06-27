28ab8bee4d41da334e1ddada76c36eb146e2447c
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-06-27 13:22:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+class Solution {
+	public int[] solution(int n) {
+		int[] answer = new int[(n + 1) / 2];
+		int j = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				answer[j] = i;
+				j++;
+			}
+		}
+
+		return answer;
+	}
+}
