c328a7ebdb4044beb1982d43876edc9d25c21c44
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-07-03 18:04:54 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+	public int solution(int n) {
+		/********************
+		 *      n(a + l)
+		 * Sn = ──────── 
+		 *          2
+		 ********************/
+		return (n / 2) * (2 + n) / 2;
+	}
+}
