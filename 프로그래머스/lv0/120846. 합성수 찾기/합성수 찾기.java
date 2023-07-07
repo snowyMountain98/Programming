fd3a6878608d6a2a5ada7f64327022102d2e8774
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-07-07 09:32:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -8,10 +8,9 @@ class Solution {
 		}
 
 		for (int i = 2; i * i <= n; i++) {
-			if (prime[i]) {
+			if (prime[i])
 				for (int j = i * i; j <= n; j += i)
 					prime[j] = false;
-			}
 		}
 
 		/* prime : [1:false, 2:true, 3:true, 4:false, 5:true, 6:false, 7:true, 8:false, 9:false, 10:false] */
