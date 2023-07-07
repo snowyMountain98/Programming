d9b574c6e263f1f8f77f5e56a1f00001de3eaa47
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-07-07 09:30:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,26 @@
+class Solution {
+	public static int solution(int n) {
+		int answer = 0;
+		boolean[] prime = new boolean[n + 1];
+
+		for (int i = 2; i <= n; i++) {
+			prime[i] = true;
+		}
+
+		for (int i = 2; i * i <= n; i++) {
+			if (prime[i]) {
+				for (int j = i * i; j <= n; j += i)
+					prime[j] = false;
+			}
+		}
+
+		/* prime : [1:false, 2:true, 3:true, 4:false, 5:true, 6:false, 7:true, 8:false, 9:false, 10:false] */
+
+		for (int i = 2; i <= n; i++) {
+			if (!prime[i])
+				answer++;
+		}
+
+		return answer;
+	}
+}
