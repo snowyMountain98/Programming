a00eacdf556c12679407d58229de18a4f68b3c46
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-07-11 18:10:56 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -6,7 +6,6 @@ class Solution {
 		else if ((double) (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0])
 				== (double) (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]))
 			return 1;
-
 		else if ((double) (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0])
 				== (double) (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]))
 			return 1;
