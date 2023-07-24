c62cbc8226ed1db9d2442d36f605b740edd4e14f
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-07-24 15:35:04 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -2,9 +2,8 @@ class Solution {
 	public int solution(int n, int t) {
 		int answer = n;
 
-		for (int i = 1; i <= t; i++) {
+		for (int i = 1; i <= t; i++)
 			answer += answer;
-		}
 
 		return answer;
 	}
