a5a6655d87ac1b16f983570a3db7ce99108c7e0f
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-08-07 17:53:57 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,19 @@
+class Solution {
+	public int solution(int[][] arr) {
+		int answer = 1;
+        
+		/********************
+		* [  5, 192,  33]
+		* [192,  72,  95]
+		* [ 33,  95, 999]
+		********************/        
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr[i].length; j++)
+				if (arr[i][j] != arr[j][i])
+					return 0;
+		}
+
+		return answer;
+	}
+}
