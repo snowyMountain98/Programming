6139309beccc10f3d358d105cdc918a36e5b0136
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-08-30 13:37:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2296 ms, Memory: 186356 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n;
+		int[][] crd; /* 좌표 */
+
+		n = scn.nextInt();
+
+		crd = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			crd[i][0] = scn.nextInt();
+			crd[i][1] = scn.nextInt();
+		}
+
+		Arrays.sort(crd, new Comparator<int[]>() {
+			public int compare(int[] a, int[] b) {
+				/******************************
+				 * 0				if x == y
+				 * less than 0		if x < y 
+				 * greater than 0	if x > y
+				 ******************************/
+				if (a[0] == b[0])
+					return Integer.compare(a[1], b[1]);
+				else
+					return Integer.compare(a[0], b[0]);
+			}
+		});
+
+		for (int i = 0; i < n; i++)
+			System.out.println(crd[i][0] + " " + crd[i][1]);
+
+		scn.close();
+	}
+
+}
