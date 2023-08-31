73472f9bce8db517a4a05c96dc0699b7c04974ae
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-08-31 16:07:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 604 ms, Memory: 56344 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n, m;
+		Map<String, String> name = new HashMap<>();
+		Map<String, String> num = new HashMap<>();
+		String pokemon, question;
+
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < n; i++) {
+			pokemon = br.readLine();
+			name.put(Integer.toString(i + 1), pokemon);
+			num.put(pokemon, Integer.toString(i + 1));
+		}
+
+		for (int i = 0; i < m; i++) {
+			question = br.readLine();
+			bw.write(name.getOrDefault(question, num.get(question)) + "\n");
+		}
+
+		bw.flush();
+	}
+
+}
