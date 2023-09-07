87512ea9c9b3402ca678b49c900dc79aff362b82
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-09-07 14:56:11 +0900

    [Silver IV] Title: 괄호, Time: 236 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		Stack<Character> stack;
+		String ps, result;
+		int t;
+
+		t = scn.nextInt();
+
+		while (t > 0) {
+			ps = scn.next();
+			stack = new Stack<>();
+			result = "YES";
+
+			CHECK: for (int i = 0; i < ps.length(); i++) {
+				switch (ps.charAt(i)) {
+				case '(':
+					stack.push('(');
+					break;
+				case ')':
+					if (stack.isEmpty() || stack.peek() != '(') {
+						result = "NO";
+						break CHECK;
+					}
+					stack.pop();
+					break;
+				}
+			}
+
+			/**************************************************
+			 * ps : (()())((()))
+			 * stack : [(] → [(, (] → [(] → [(, (] → [(] → []
+			 * → [(] → [(, (] → [(, (, (] → [(, (] → [(] → []
+			 **************************************************/
+
+			if (!stack.isEmpty())
+				result = "NO";
+
+			System.out.println(result);
+
+			t--;
+		}
+	}
+
+}
