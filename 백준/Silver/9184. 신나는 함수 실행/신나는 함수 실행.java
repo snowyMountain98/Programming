d3565370482643318a03ffe40f7c13b350bbb404
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2023-10-05 10:05:08 +0900

    Update 신나는 함수 실행.java

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -4,23 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		long[][][] w;
+		int[][][] w;
 		int a, b, c;
 
-		w = new long[101][101][101];
+		w = new int[101][101][101];
 
 		for (int i = 0; i <= 100; i++) {
 			for (int j = 0; j <= 100; j++)
 				for (int k = 0; k <= 100; k++) {
 					if (i <= 50 || j <= 50 || k <= 50)
 						w[i][j][k] = 1;
-					else if (i > 70 || j > 70 || k > 70)
-						w[i][j][k] = 1048576;
 					else if (i < j && j < k)
 						w[i][j][k] = w[i][j][k - 1] + w[i][j - 1][k - 1] - w[i][j - 1][k];
 					else
 						w[i][j][k] = w[i - 1][j][k] + w[i - 1][j - 1][k] + w[i - 1][j][k - 1] - w[i - 1][j - 1][k - 1];
 				}
+		}
+
+		for (int i = 51; i <= 100; i++) {
+			for (int j = 51; j <= 100; j++)
+				for (int k = 51; k <= 100; k++)
+					if (i > 70 || j > 70 || k > 70)
+						w[i][j][k] = w[70][70][70];
 		}
 
 		do {
