26d372139f6aa8cdfa4bdf6d5c5357c9c12a176c
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2024-07-18 21:59:55 +0900

    [Gold IV] Title: 행렬 제곱, Time: 148 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -1,38 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+	public static void main(String[] args) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		long B;
 		int[][] A, rslt;
 
-		N = scn.nextInt();
-		B = scn.nextLong();
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		B = Long.parseLong(st.nextToken());
 
 		A = new int[N][N];
 		rslt = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				A[i][j] = scn.nextInt();
+				A[i][j] = Integer.parseInt(st.nextToken());
 				A[i][j] %= 1000;	// 행렬의 각 원소는 1,000보다 작거나 같은 자연수 또는 0
 			}
 		}
 
 		rslt = power(A, B);
-
+        
 		/**************************************************
-		 * 	7 10  →  37  54  →  199 290  →  69 558 
+		 *  7 10  →  37  54  →  199 290  →   69 558 
 		 * 15 22     81 118     435 634     337 406 
 		 **************************************************/
 
-		for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++)
-				System.out.print(rslt[i][j] + " ");
-			System.out.println();
+				bw.write(rslt[i][j] + " ");
+			bw.write("\n");
 		}
+		
+		bw.flush();
 	}
 
 	public static int[][] power(int[][] A, long B) {
