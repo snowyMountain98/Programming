b91080cfc1a93fb9edc7fc96539a6e2964545667
이서라 <46704553+snowyMountain98@users.noreply.github.com>
2024-10-31 07:52:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 116 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n;
+		int[] tiling = new int[1001];
+		;
+
+		n = Integer.parseInt(br.readLine());
+
+		tiling[1] = 1;
+		tiling[2] = 2;
+
+		for (int i = 3; i <= n; i++)
+			tiling[i] = (tiling[i - 1] + tiling[i - 2]) % 10007;
+
+		/**************************************************
+		 * tiling[i - 2]	tiling[i - 1]
+		 * ┌─┬─┬─┲━━━┓      ┌─┬─┬─┬─┲━┓
+		 * │ │ │ ┣━━━┫      │ │ │ │ ┃ ┃
+		 * └─┴─┴─┺━━━┛      └─┴─┴─┴─┺━┛
+		 * ┌─┬───┲━━━┓      ┌───┬───┲━┓
+		 * │ ├───╊━━━┫      ├───┼───┨ ┃
+		 * └─┴───┺━━━┛      └───┴───┺━┛
+		 * ┌───┬─┲━━━┓      ┌─┬───┬─┲━┓
+		 * ├───┤ ┣━━━┫      │ ├───┤ ┃ ┃
+		 * └───┴─┺━━━┛      └─┴───┴─┺━┛
+		 *                  ┌─┬─┬───┲━┓
+		 *                  │ │ ├───┨ ┃
+		 *                  └─┴─┴───┺━┛
+		 *                  ┌───┬─┬─┲━┓
+		 *                  ├───┤ │ ┃ ┃
+		 *                  └───┴─┴─┺━┛
+		 **************************************************/
+
+		bw.write(tiling[n] + "\n");
+		bw.flush();
+	}
+
+}
